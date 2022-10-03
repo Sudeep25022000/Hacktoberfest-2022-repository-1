d365431a5804fc2147f13e702a5c9747ed6545ac
Bhumi54321 <102283681+Bhumi54321@users.noreply.github.com>
2022-10-03 15:02:12 +0530

    Add files via upload

---
+++ FIBONACCI SERIES.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=0,b=1,c,i,n;
+printf("Enter number of terms:");
+scanf("%d",&n);
+printf("%d  %d",a,b);
+for(i=3;i<=n;i++)
+{
+c=a+b;
+printf("  %d",c);
+a=b;
+b=c;
+}
+return 0;
+getch();
+}
+
